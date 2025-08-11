9526b7958a7debcd79972c2c17c74beb03b31edf
Hakutaka_id <rachmad.alfio@gmail.com>
2025-08-11 12:24:16 +0700

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
 API_ID = int(os.environ.get("API_ID", ""))
 
-# Your API Hash from my.telegram.org
+
 API_HASH = os.environ.get("API_HASH", "")
 
 # Your Owner / Admin Id For Broadcast 
